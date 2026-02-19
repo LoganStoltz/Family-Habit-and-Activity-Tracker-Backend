6f91d2a4c280a4f630a3773a279ba816f3b2d812
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2026-02-18 16:54:34 -0800

    .
    .

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://familyhabittracker.com', 'https://www.familyhabittracker.com'
+    origins 'https://familyhabittracker.com', 'https://www.familyhabittracker.com', 'localhost'
 
     resource '*',
       headers: :any,
